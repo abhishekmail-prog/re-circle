62c11563f5ee7248c755ee9f7aadac4105fba7cb
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-09-13 14:43:53 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/recircle/config/CorsConfig.java
+++ backend/src/main/java/com/recircle/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://10.252.126.192:5173", "https://recircle-frontend.onrender.com")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://10.252.126.192:5173", "https://recircle-backend-b1gl.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
